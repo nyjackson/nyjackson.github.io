5b5d149ad8bf073e28545fb08a5b5a3bbbc9a3a1
Nyala Jackson <nymjackson@gmail.com>
2026-05-18 21:50:06 -0400

    ddeployment fixes

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: './', // Using './' ensures paths work on any GitHub Pages URL
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
 })
